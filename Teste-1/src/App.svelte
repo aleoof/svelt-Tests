b02c2fe57bc7cf4e23edb1784a7222b8be8f330e
Alexandre <alexandre_osvaldo@hotmail.com>
2019-12-04 16:30:37 -0200

    first commit

---
+++ Teste-1/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Template from "./template.svelte";
+
+  let items = [];
+  let value = 0;
+
+  function handleAddItem() {
+    value += 1;
+    items[items] = items.push({ name: `teste ${value}` });
+    console.log(items);
+  }
+</script>
+
+<style>
+  main {
+    text-align: left;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 1em;
+  }
+</style>
+
+<main>
+  <button on:click={handleAddItem}>Adicionar item</button>
+
+  {#each items as item}
+    <Template name={item.name} />
+  {/each}
+
+</main>
